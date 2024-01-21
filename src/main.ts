2e3de9aecde64cc00ae65e14a097007560b25775
KIMSIHUN <tlgns14@nate.com>
2024-01-21 18:22:34 +0900

    🎉 first commit

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        package: 'hero',
+        protoPath: join(__dirname, 'proto/your-service.proto'),
+      },
+    },
+  );
+  await app.listen();
+}
+bootstrap();
