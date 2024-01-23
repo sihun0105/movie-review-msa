3c63f36ced543688f915ef807e084b686b38ef5c
KIMSIHUN <tlgns14@nate.com>
2024-01-23 20:03:00 +0900

    🔥 연습파일 제거

--- src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { HeroesController } from './hero/heroes.controller';
-import { HeroClientModule } from './hero/client/hero.client.module';
-
-@Module({
-  imports: [HeroClientModule],
-  controllers: [HeroesController],
-})
-export class AppModule {}
