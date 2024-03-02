755cc3f79f6401ee885b98aae2c014543b4245ca
KIMSIHUN <tlgns14@nate.com>
2024-03-02 14:16:13 +0900

    🔥 데이터베이스 서비스 제거

--- apps/database/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { DatabaseModule } from './database.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(DatabaseModule);
-  await app.listen(3002);
-}
-bootstrap();
