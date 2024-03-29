adff4cb1c34db2cd356d76eb47b275847d37df8a
KIMSIHUN <tlgns14@nate.com>
2024-03-29 15:35:39 +0900

    🐛 movie 서비스 의존성 주입

--- apps/cron/src/cron.service.ts
+++ apps/cron/src/cron.service.ts
@@ -1,9 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import {
+  MOVIE_PACKAGE_NAME,
+  MOVIE_SERVICE_NAME,
+  MovieServiceClient,
+} from '@app/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientGrpc } from '@nestjs/microservices';
 import { Cron } from '@nestjs/schedule';
 
 @Injectable()
 export class CronService {
-  constructor(private readonly movieService: MovieService) {}
+  private movieService: MovieServiceClient;
+  constructor(
+    @Inject(MOVIE_PACKAGE_NAME)
+    private client: ClientGrpc,
+  ) {}
+  onModuleInit() {
+    this.movieService =
+      this.client.getService<MovieServiceClient>(MOVIE_SERVICE_NAME);
+  }
 
   @Cron('0 0 * * *')
   getMoviedata() {
@@ -13,6 +27,6 @@ export class CronService {
       nowdate.getFullYear().toString() +
       (nowdate.getMonth() + 1).toString().padStart(2, '0') +
       (nowdate.getDate() - 1).toString().padStart(2, '0');
-    this.movieService.fetchMovies(formattedDate);
+    this.movieService.fetchMovies({ fetchDate: formattedDate });
   }
 }
