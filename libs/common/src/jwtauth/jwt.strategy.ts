15364ec76c9cd1353a28ef3746211526348f93d6
KIMSIHUN <tlgns14@nate.com>
2024-04-03 17:25:56 +0900

    🎨 JWT가드 추가

---
+++ libs/common/src/jwtauth/jwt.strategy.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { JwtPayload } from '@app/common/types';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_ACCESS_SECRET,
+    });
+  }
+  async validate(payload: JwtPayload): Promise<JwtPayload> {
+    return payload;
+  }
+}
--- libs/common/src/types/index.ts
+++ libs/common/src/types/index.ts
@@ -3,3 +3,4 @@ export * from './user';
 export * from './reply';
 export * from './movie';
 export * from './rpc-exception';
+export * from './jwt-payload';
