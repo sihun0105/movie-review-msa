72ceaddac1b5e6201c67ae3ce791f0d9445666f2
KIMSIHUN <tlgns14@nate.com>
2024-05-31 15:23:30 +0900

    🎨 영화 상세정보 타입 정의

---
+++ libs/common/src/types/movie-detail-response.ts
@@ -0,0 +1,16 @@
+export interface MovieDetailData {
+  title: string; // 제목
+  titleEng: string; // 영어제목
+  titleOrg: string; // 원제
+  directorNm: string; // 감독이름
+  directorEnNm: string; // 감독이름(영어)
+  actorNm: string; // 배우이름
+  plot: string; // 줄거리
+  company: string; // 제작사
+  nation: string; // 국가
+  rating: string; // 등급
+  genre: string; // 장르
+  releaseDate: string; // 개봉일
+  runtime: string; // 상영시간
+  posterUrl: string; // 포스터 URL
+}
