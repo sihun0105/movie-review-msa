08ab53d464936292498b98c8b482562d25afd62f
KIMSIHUN <tlgns14@nate.com>
2024-06-29 17:33:10 +0900

    🔥 remove rabitMQ

--- apps/api-gateway/src/app.module.ts
+++ apps/api-gateway/src/app.module.ts
@@ -1,15 +1,10 @@
+import { JwtStrategy } from '@app/common/guards/jwtauth/jwt.strategy';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { MovieModule } from './movie/movie.module';
 import { ReplyModule } from './reply/reply.module';
 import { UserModule } from './user/user.module';
-import { MovieModule } from './movie/movie.module';
-import { JwtStrategy } from '@app/common/guards/jwtauth/jwt.strategy';
-import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
-
-const rabbitHost = '127.0.0.1';
-const rabbitPort = '5672';
-const uri = `amqp://guest:guest@${rabbitHost}:${rabbitPort}`;
 
 @Module({
   imports: [
@@ -21,18 +16,7 @@ const uri = `amqp://guest:guest@${rabbitHost}:${rabbitPort}`;
       cache: true,
       isGlobal: true,
     }),
-    RabbitMQModule.forRootAsync(RabbitMQModule, {
-      useFactory: () => ({
-        exchanges: [
-          {
-            name: 'hello',
-            type: 'topic',
-          },
-        ],
-        uri,
-        connectionInitOptions: { wait: true, reject: true, timeout: 3000 },
-      }),
-    }),
+
     AuthModule,
     UserModule,
     ReplyModule,
