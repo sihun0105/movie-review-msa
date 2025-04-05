9275810f22fddaa8a4bcd139f19cf594d3f4ce30
KIMSIHUN <tlgns14@nate.com>
2025-04-05 13:03:24 +0900

    🎨

--- prisma/generated/mysql/edge.js
+++ prisma/generated/mysql/edge.js
@@ -252,6 +252,7 @@ const config = {
     "db"
   ],
   "activeProvider": "mysql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
--- prisma/generated/mysql/index.js
+++ prisma/generated/mysql/index.js
@@ -253,6 +253,7 @@ const config = {
     "db"
   ],
   "activeProvider": "mysql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
